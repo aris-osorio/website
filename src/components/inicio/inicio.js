04f5a02346df856d3684536295f8da03e2e174c6
aris-osorio <aris.osorio@alumnos.udg.mx>
2021-02-25 02:26:34 -0600

    quitar formula tamano 100vh

--- src/components/inicio/inicio.js
+++ src/components/inicio/inicio.js
@@ -2,9 +2,8 @@ import './inicio.css'
 import Typed from 'react-typed';
 
 export default function Inicio() {
-    // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+    
     let vh = window.innerHeight * 0.01;
-    // Then we set the value in the --vh custom property to the root of the document
     document.documentElement.style.setProperty('--vh', `${vh}px`);
 
     return (
