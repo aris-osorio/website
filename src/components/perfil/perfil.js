649d2bb750f930e0f222e4e8ce543ac307bc84c0
aris-osorio <aris.osorio@alumnos.udg.mx>
2021-02-24 21:49:07 -0600

    creando componente inicio y perfil

---
+++ src/components/perfil/perfil.js
@@ -0,0 +1,21 @@
+import './perfil.css'
+
+export default function Perfil() {
+    // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+    let vh = window.innerHeight * 0.01;
+    // Then we set the value in the --vh custom property to the root of the document
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+    
+    return (
+        <section id="inicio">
+            <div className="bg-perfil">
+                <div className="">
+                    <div className="">
+                        <h1 className="">Aris Osorio</h1>
+                        <h2 className="">Desarrollador</h2>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+}
